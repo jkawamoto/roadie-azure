729b7d751661cfe19fcac802b1428bac3363457b
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2017-04-12 02:42:15 -0400

    Implement application information.

---
+++ version.go
@@ -0,0 +1,33 @@
+//
+// version.go
+//
+// Copyright (c) 2017 Junpei Kawamoto
+//
+// This file is part of Roadie Azure.
+//
+// Roadie Azure is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Roadie Azure is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Roadie Azure. If not, see <http://www.gnu.org/licenses/>.
+//
+
+package main
+
+const (
+	// Name of this command.
+	Name string = "roadie-azure"
+	// Version of this command.
+	Version string = "0.1.0"
+	// Author defines the author of this program.
+	Author string = "Junpei Kawamoto"
+	// Email defines an email address of the author.
+	Email string = "kawamoto.junpei@gmail.com"
+)
