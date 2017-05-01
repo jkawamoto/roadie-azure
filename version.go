d1ee94398916f2346a912a07bb63d507eba148b0
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2017-05-01 19:03:10 -0400

    Bump version: 0.3.0 → 0.3.1

--- version.go
+++ version.go
@@ -25,7 +25,7 @@ const (
 	// Name of this command.
 	Name string = "roadie-azure"
 	// Version of this command.
-	Version string = "0.3.0"
+	Version string = "0.3.1"
 	// Author defines the author of this program.
 	Author string = "Junpei Kawamoto"
 	// Email defines an email address of the author.
